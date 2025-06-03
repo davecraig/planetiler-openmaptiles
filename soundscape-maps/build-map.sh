d4a076b20ac425d58ef1750213fadc11eb223123
Dave Craig <davecraig@unbalancedaudio.com>
2025-06-03 10:09:33 +0100

    Add build-planet-map script
    build-planet-map is what is being used to build the maps for soundscape. It takes
    two arguments, one for a temp directory, and one for an output directory. When I
    run it, they are the same, but it may be more efficient to have these on different
    drives.
    The latest planet.osm file is downloaded from AWS which is generally the fastest
    and has zero cost for OSM.

--- soundscape-maps/build-map.sh
+++ soundscape-maps/build-map.sh
@@ -1,2 +1,2 @@
 mkdir -p map-to-serve
-java -Xmx30g -jar ../target/planetiler-openmaptiles-3.15.1-SNAPSHOT-with-deps.jar --force --download --area=$1 --fetch-wikidata --output=map-to-serve/$1.pmtiles --maxzoom=15 --render_maxzoom=15 --simplify-tolerance-at-max-zoom=-1
+java -Xmx30g -jar ../target/planetiler-openmaptiles-3.15.1-SNAPSHOT-with-deps.jar --force --download --area=$1 --fetch-wikidata --output=map-to-serve/$1.pmtiles --maxzoom=15 --render_maxzoom=15 --simplify-tolerance-at-max-zoom=-1 --min_feature_size_at_max_zoom=0
